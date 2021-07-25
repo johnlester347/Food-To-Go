8643187450226286b56553560f4872f971474cc0
JohnLester <jonhlesterbalucas.pamplona@gmail.com>
2021-07-25 07:16:46 +0700

    Adding order now and its functionality

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ const navBtn = document.querySelector(".btn-nav");
 
 navBtn.addEventListener('click', function (e) {
     if (navList.classList.contains('nav-phone_inactive')) {
-        document.querySelector('#Nav button i').classList.add('fa-bars');
-        document.querySelector('#Nav button i').classList.remove('fa-times');
+        document.querySelector('#Nav button i').classList.remove('fa-bars');
+        document.querySelector('#Nav button i').classList.add('fa-times');
         navList.classList.toggle('nav-phone_inactive');
         navList.classList.toggle('nav-phone_active');
     } else {
-        document.querySelector('#Nav button i').classList.remove('fa-bars');
-        document.querySelector('#Nav button i').classList.add('fa-times');
+        document.querySelector('#Nav button i').classList.add('fa-bars');
+        document.querySelector('#Nav button i').classList.remove('fa-times');
         navList.classList.toggle('nav-phone_active');
         navList.classList.toggle('nav-phone_inactive');
     }
